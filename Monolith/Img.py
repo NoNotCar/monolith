3b55accdffa2e9af0ee24411785f92c386a11242
NoNotCar <13haslat@gmail.com>
2015-08-14 11:52:22 +0100

    Linux Compatibility

--- Monolith/Img.py
+++ Monolith/Img.py
@@ -7,26 +7,27 @@ import pygame
 import os
 pygame.init()
 pygame.mixer.init()
-loc = os.path.dirname(os.getcwd())+"\Assets\\"
+np=os.path.normpath
+loc = os.path.dirname(os.getcwd())+"\\Assets\\"
 def imgret2(fil):
-    return pygame.transform.scale2x(pygame.image.load(loc + fil).convert_alpha())
+    return pygame.transform.scale2x(pygame.image.load(np(loc + fil)).convert_alpha())
 def imgret2b(fil):
-    img=pygame.image.load(loc + fil).convert_alpha()
+    img=pygame.image.load(np(loc + fil)).convert_alpha()
     return pygame.transform.scale(img,(img.get_width()*2,img.get_height()*2))
 def imgret32(fil):
-    return pygame.transform.scale(pygame.image.load(loc + fil).convert_alpha(),(32,32))
+    return pygame.transform.scale(pygame.image.load(np(loc + fil)).convert_alpha(),(32,32))
 def imgret32x16(fil):
-    return pygame.transform.scale(pygame.image.load(loc + fil).convert_alpha(),(32,16))
+    return pygame.transform.scale(pygame.image.load(np(loc + fil)).convert_alpha(),(32,16))
 def imgretrect(fil,x,y):
-    return pygame.transform.scale(pygame.image.load(loc + fil), (x, y))
+    return pygame.transform.scale(pygame.image.load(np(loc + fil)), (x, y))
 def imgretsrect(fil,x,y):
-    return pygame.transform.smoothscale(pygame.image.load(loc + fil), (x, y))
+    return pygame.transform.smoothscale(pygame.image.load(np(loc + fil)), (x, y))
 def imgtransrect(fil,x,y):
     return pygame.transform.scale(fil, (x, y))
 def sndget(fil):
-    return pygame.mixer.Sound(loc+"Sounds\\" + fil)
+    return pygame.mixer.Sound(np(loc+"Sounds\\" + fil))
 def imgret(img):
-    return pygame.image.load(loc+img).convert_alpha()
+    return pygame.image.load(np(loc+img)).convert_alpha()
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
     orig_rect = image.get_rect()
@@ -36,7 +37,7 @@ def rot_center(image, angle):
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 def musply(mus):
-    pygame.mixer.music.load(loc+"Music\\"+mus)
+    pygame.mixer.music.load(np(loc+"Music\\"+mus))
     pygame.mixer.music.play(-1)
 def Fsize(size):
     return pygame.font.Font(pygame.font.get_default_font(), size)
